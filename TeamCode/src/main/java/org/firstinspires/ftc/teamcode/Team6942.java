a90eb3c194eca6f1c84b84efa01ae73cbf24bcc2
Team6942 <Wbrobotics6942@gmail.com>
2018-10-19 15:36:06 -0400

    bbb

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team6942.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team6942.java
@@ -1,12 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-/**
- * Created by gdesimone on 10/19/2018.
- */
 
+@TeleOp
 public class Team6942 extends LinearOpMode {
     private DcMotor flipper;
     int x;
@@ -17,6 +16,9 @@ public class Team6942 extends LinearOpMode {
         while (opModeIsActive()){
 
          x = flipper.getCurrentPosition();
+         flipper.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+         flipper.setTargetPosition(-500);
+         flipper.setPower(1);
          telemetry.addData("",x);
          telemetry.update();
 
