c9ed4d3c15a926f7ac15a7cad817a2b125ed1aec
Team6942 <Wbrobotics6942@gmail.com>
2018-10-24 14:21:46 -0400

    bbb

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ResetMotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ResetMotor.java
@@ -4,9 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-/**
- * Created by gdesimone on 10/22/2018.
- */
+
 @TeleOp
 public class ResetMotor  extends LinearOpMode {
     private DcMotor flipper;
