eebb8219cd49b062c55ad94c54383fe81638f8b2
Team6942 <Wbrobotics6942@gmail.com>
2018-10-29 14:29:29 -0400

    bbb

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AlexanderGherardiRobat.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AlexanderGherardiRobat.java
@@ -1,31 +1,4 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+
 
 package org.firstinspires.ftc.teamcode;
 
@@ -42,7 +15,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.opencv.*;
-@TeleOp(name="alexander is ta bomb", group="DogeCV")
+@TeleOp()
 
 public class AlexanderGherardiRobat extends OpMode
 {
@@ -56,10 +29,12 @@ public class AlexanderGherardiRobat extends OpMode
     @Override
     public void init() {
         telemetry.addData("Status", "alexander is ta bomb");
+        //Initialise motors
         fl = hardwareMap.get(DcMotor.class,"fl");
         br = hardwareMap.get(DcMotor.class,"br");
         bl = hardwareMap.get(DcMotor.class,"bl");
         fr = hardwareMap.get(DcMotor.class,"fr");
+        //Initialise detectors
         detector = new SamplingOrderDetector();
         detector.init(hardwareMap.appContext, CameraViewDisplay.getInstance());
         detector.useDefaults();
@@ -67,16 +42,6 @@ public class AlexanderGherardiRobat extends OpMode
         goldAlignDetector = new GoldAlignDetector();
         goldAlignDetector.init(hardwareMap.appContext, CameraViewDisplay.getInstance());
         goldAlignDetector.useDefaults();
-
-        detector.downscale = 0.4; // How much to downscale the input frames
-
-        // Optional Tuning
-        detector.areaScoringMethod = DogeCV.AreaScoringMethod.MAX_AREA; // Can also be PERFECT_AREA
-        //detector.perfectAreaScorer.perfectArea = 10000; // if using PERFECT_AREA scoring
-        detector.maxAreaScorer.weight = 0.001;
-
-        detector.ratioScorer.weight = 15;
-        detector.ratioScorer.perfectRatio = 1.0;
 
         detector.enable();
 
@@ -87,9 +52,7 @@ public class AlexanderGherardiRobat extends OpMode
     public void init_loop() {
     }
 
-    /*
-     * Code to run ONCE when the driver hits PLAY
-     */
+
     @Override
     public void start() {
 
@@ -98,15 +61,15 @@ public class AlexanderGherardiRobat extends OpMode
 
     @Override
     public void loop() {
-        telemetry.addData("Current Order" , detector.getCurrentOrder().toString()); // The current result for the frame
-        telemetry.addData("Last Order" , detector.getLastOrder().toString()); // The last known result
+        telemetry.addData("Current Order" , detector.getCurrentOrder().toString());
+        telemetry.addData("Last Order" , detector.getLastOrder().toString());
         if (detector.getCurrentOrder() == SamplingOrderDetector.GoldLocation.CENTER) {
             //todo do thing hear
         } if (detector.getCurrentOrder() == SamplingOrderDetector.GoldLocation.LEFT) {
             //todo do thing hear
             double x = goldAlignDetector.getXPosition();
             if (x < 0){
-                telemetry.addData("Gold Current Order" , "bbbb"); // The current result for the frame
+                telemetry.addData("Gold Current Order" , "bbbb");
 
             }
 
@@ -114,7 +77,7 @@ public class AlexanderGherardiRobat extends OpMode
             //todo do thing hear
             double x = goldAlignDetector.getXPosition();
             if (0 < x){
-                telemetry.addData("Gold Current Order" , "ccccc"); // The current result for the frame
+                telemetry.addData("Gold Current Order" , "ccccc");
 
             }
         }
@@ -122,9 +85,7 @@ public class AlexanderGherardiRobat extends OpMode
         telemetry.update();
     }
 
-    /*
-     * Code to run ONCE after the driver hits STOP
-     */
+
     @Override
     public void stop() {
         detector.disable();
